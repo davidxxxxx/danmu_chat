609a7f1657f9512652a69e73a47d66318f77c5dc
jiexu <jiexu@JIEXU2L>
2023-05-05 15:49:05 +0800

    Add project files.

---
+++ danmu_chat/danmu_start.py
@@ -0,0 +1,80 @@
+﻿from pydouyu.client import Client
+import time
+import sys
+import sqlite3
+from threading import local, Thread
+from queue import Queue
+import matplotlib.pyplot as plt
+
+class DB:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.local_data = local()
+        self.initialize_db()
+
+    def get_conn(self):
+        if not hasattr(self.local_data, "conn"):
+            self.local_data.conn = sqlite3.connect(self.db_name)
+        return self.local_data.conn
+
+    def store_msg(self, msg):
+        conn = self.get_conn()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO chat_messages (user, message, timestamp) VALUES (?, ?, ?)",
+                       (msg['nn'], msg['txt'], time.time()))
+        conn.commit()
+
+    def initialize_db(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS chat_messages (
+            id INTEGER PRIMARY KEY,
+            user TEXT,
+            message TEXT,
+            timestamp REAL)
+        ''')
+        conn.commit()
+
+
+db = DB("chat_messages.db")
+
+count_queue = Queue()
+times = []
+counts = []
+
+def chatmsg_handler(msg):
+    db.store_msg(msg)
+    count_queue.put(1)  # Push a count into the queue
+    output = time.strftime("[%Y-%m-%d %H:%M:%S] ", time.localtime()) + msg['nn'] + ": " + msg['txt']
+    print(output)
+    sys.stdout.flush()
+
+def uenter_handler(msg):
+    output = time.strftime("[%Y-%m-%d %H:%M:%S] ", time.localtime()) + msg['nn'] + " 进入了直播间"
+    print(output)
+    sys.stdout.flush()
+
+def live_plot():
+    plt.ion()
+    while True:
+        count = 0
+        start_time = time.time()
+        while time.time() - start_time < 30:
+            while not count_queue.empty():
+                count += count_queue.get()
+            time.sleep(1)
+        times.append(time.strftime("%H:%M:%S", time.localtime()))
+        counts.append(count)
+        plt.clf()
+        plt.plot(times, counts)
+        plt.draw()
+        plt.pause(0.1)
+
+plot_thread = Thread(target=live_plot)
+
+c = Client(room_id=52876)
+c.add_handler('chatmsg', chatmsg_handler)
+c.add_handler('uenter', uenter_handler)
+plot_thread.start()
+c.start()
---
+++ danmu_chat/test.py
@@ -0,0 +1,111 @@
+﻿import threading
+import queue
+import time
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.animation import FuncAnimation
+from matplotlib.dates import date2num, DateFormatter
+from collections import deque
+from datetime import datetime, timedelta
+
+from pydouyu.client import Client
+import sqlite3
+
+# Queues for sharing data between threads
+data_queue = queue.Queue()
+db_queue = queue.Queue()
+
+# Deque for storing messages
+messages = deque()
+
+def db_worker():
+    conn = sqlite3.connect('danmu.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS chat_messages (
+            id INTEGER PRIMARY KEY,
+            user TEXT NOT NULL,
+            message TEXT NOT NULL,
+            timestamp TIMESTAMP NOT NULL
+        );
+    ''')
+
+    while True:
+        msg = db_queue.get()
+        if msg is None:  # exit signal
+            break
+        cursor.execute("INSERT INTO chat_messages (user, message, timestamp) VALUES (?, ?, ?)",
+                       (msg['nn'], msg['txt'], datetime.now()))
+        conn.commit()
+
+    conn.close()
+
+def chatmsg_handler(msg):
+    msg['timestamp'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    db_queue.put(msg)
+    messages.append(msg)
+    output = time.strftime("[%Y-%m-%d %H:%M:%S] ", time.localtime()) + msg['nn'] + ": " + msg['txt']
+    print(output)
+
+def data_collector(room_id):
+    c = Client(room_id=room_id)
+    c.add_handler('chatmsg', chatmsg_handler)
+    c.start()
+
+
+def plotter():
+    fig, ax = plt.subplots()
+    xdata, ydata = [], []
+    ln, = plt.plot([], [], 'r-')
+
+    def count_per_interval(interval):
+        now = datetime.now()
+        delta = timedelta(seconds=interval)
+        counter = 0
+        while len(messages) > 0:  # as long as there are messages left
+            timestamp_str = messages[0].get('timestamp')
+            if timestamp_str is not None:
+                timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
+                if now - timestamp <= delta:
+                    counter += 1
+                    messages.popleft()  # remove this message
+                else:
+                    break  # if this message is out of interval, so are the rest
+        return counter
+
+    def init():
+        ax.set_ylim(0, 5)
+        ax.set_xlim([datetime.now(), datetime.now() + timedelta(seconds=30)])
+        del xdata[:]
+        del ydata[:]
+        return ln,
+
+    def update(frame):
+        counter = count_per_interval(30)
+        xdata.append(datetime.now())  # append current time
+        ydata.append(counter)
+        ln.set_data(xdata, ydata)
+        ax.relim()
+        ax.autoscale_view()
+        ax.xaxis_date()
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+        ax.yaxis.get_major_locator().set_params(integer=True)  # ensure y-axis uses integer ticks
+        return ln,
+  #  ani = FuncAnimation(fig, update, interval=30000, blit=True, cache_frame_data=False)
+
+    ani = FuncAnimation(fig, init_func=init, func=update, frames=None, interval=30000, blit=True)
+    plt.show()
+
+
+db_thread = threading.Thread(target=db_worker)
+db_thread.start()
+
+room_id = 73965  # replace with your room id
+collector_thread = threading.Thread(target=data_collector, args=(room_id,))
+collector_thread.start()
+
+plotter()
+
+db_queue.put(None)
+db_thread.join()
